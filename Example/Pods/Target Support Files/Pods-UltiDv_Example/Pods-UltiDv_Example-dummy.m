c038d6249f14e135f1cdb847a072de60abd8c54b
David Viloria <david.viloria0424@gmail.com>
2022-06-25 19:42:57 -0500

    Extension de Uiview

---
+++ Example/Pods/Target Support Files/Pods-UltiDv_Example/Pods-UltiDv_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UltiDv_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UltiDv_Example
+@end
---
+++ Example/Pods/Target Support Files/UltiDv/UltiDv-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UltiDv : NSObject
+@end
+@implementation PodsDummy_UltiDv
+@end
