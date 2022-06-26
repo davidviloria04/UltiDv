a03b55a9aaa1c1cf76b5f7d6150b2a97f0c0578d
David Viloria <david.viloria0424@gmail.com>
2022-06-25 19:54:49 -0500

    Ejemplo añadido

--- Example/UltiDv/ViewController.swift
+++ Example/UltiDv/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import UltiDv
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainView.backgroundColor = .green
+        mainView.addShadow()
+        mainView.addCornerRadius()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
